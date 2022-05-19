794c6cbc001ff4614555892030c71badf61dc131
Mario Yepes <marioy47@gmail.com>
2022-05-19 15:45:40 -0500

    Updating treesitter config since the _maintained_ option no longer works

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,15 +1,15 @@
 -- lua/config/nvim-treesitter.lua
 
 require("nvim-treesitter.configs").setup({
-    -- To install additional languages, do: `:TSInstall <mylang>`. `:TSInstall maintained` to install all maintained
-    ensure_installed = "maintained",
+    -- To install additional languages, do: `:TSInstall <mylang>`.
+    ensure_installed = "all",
     sync_installed = true,
     highlight = {
         enable = true, -- This is a MUST
-        additional_vim_regex_highlighting = { "php" },
+        additional_vim_regex_highlighting = false,
     },
     indent = {
-        enable = false, -- Really breaks stuff if true
+        enable = true, -- If indent does not work in PHP, set `additional_vim_regex_highlighting = { "php" } `
     },
     incremental_selection = {
         enable = true, -- Enable selection of CODE blocks
@@ -22,11 +22,8 @@ require("nvim-treesitter.configs").setup({
     },
     rainbow = {
         enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
     },
 })
 
