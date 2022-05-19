8c5908822f3ae24ce615c383f1acb645bb50a70e
Mario Yepes <marioy47@gmail.com>
2022-05-19 16:11:29 -0500

    Upgrading and reordering nvim-tree configuration

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,24 +1,39 @@
 -- lua/config/nvim-tree.lua
-vim.g.nvim_tree_indent_markers = 1
+-- vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE", "package.json", "composer.json" }
 vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
 
 require("nvim-tree").setup({
-    diagnostics = {
-        enable = true,
-    },
-    git = {
-        ignore = false,
-    },
     view = {
-        width = 40,
+        width = 45,
         side = "right",
         mappings = {
             list = {
                 { key = "<C-k>", action = "" },
                 { key = "i", action = "toggle_file_info" },
             },
+        },
+    },
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+        icons = {
+            git_placement = "after",
+        },
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+    },
+    git = {
+        ignore = false,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+            resize_window = true,
         },
     },
 })
