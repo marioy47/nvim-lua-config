5f527f496593fc06bb11de6fa44f9f832020b888
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-05-19 19:14:29 +0800

    fix lualine.lua

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -4,13 +4,14 @@ require("lualine").setup({
     options = {
         icons_enabled = true,
         theme = "auto",
-        -- component_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = {},
         always_divide_middle = true,
+        globalstatus = false,
     },
     sections = {
-        lualine_a = {},
+        lualine_a = {'mode'},
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = {
             {
