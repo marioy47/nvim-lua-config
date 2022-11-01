0136acc84a4aff953754d0b21ae92a371a5ad2c7
Mario Yepes <marioy47@gmail.com>
2022-11-01 17:16:12 -0500

    Adding new options and upgrading autocommands

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- init.lua
 
+-- LSP servers to install by default (you can add more with :LspInstallInfo)
 vim.g.lsp_servers = {
     "bashls",
     "emmet_ls",
@@ -16,27 +17,14 @@ vim.g.lsp_servers = {
 -- Vim native options. Make it more modern
 require("options")
 
--- All keymaps in one file for easier research
-require("keymaps")
-
 -- Vim commands and autocommands
 require("commands")
 
+-- All keymaps in one file for easier research
+-- require("keymaps")
+
 -- Plugins and plugin configuration
-require("plugins")
+-- require("plugins")
 
--- "Try" to setup the color scheme
--- vim.cmd([[silent! colorscheme nightfox]])
-
--- Possible values: storm, night and day
--- vim.g.tokyonight_style = "night"
-vim.cmd[[silent! colorscheme tokyonight]]
-
--- vim.g.material_style = "deep ocean" -- Possible values: palenight, oceanic, deep ocean, darker, ligher
--- vim.cmd([[silent! colorscheme material]])
-
--- vim.g.oceanic_transparent_bg = 1
--- vim.g.oceanic_italic_comments = 1
--- vim.cmd[[silent! colorscheme oceanicnext]]
-
--- vim.cmd([[silent! colorscheme github_dimmed]])
+-- Execute `:echo getcompletion('', 'color')` for a list of color schemes
+vim.cmd([[silent! colorscheme tokyonight]])
--- lua/commands.lua
+++ lua/commands.lua
@@ -1,69 +1,48 @@
 -- commands.lua
 
--- Hightlight any yanked line
-vim.cmd([[
-  augroup highlight-text-on-yank
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]])
+vim.cmd("autocmd!")
 
--- When re-open a file, place the cursor at the last position
-vim.cmd([[
-augroup vimrc-remember-cursor-position
-  autocmd!
-  autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-augroup END
-]])
+--[[
+-- Command that get executed automatically
+-- ]]
+vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    desc = "Hightlight any yanked line",
+})
 
--- If NeoVim is opened without parammeter, start NetRw on curren dir
-vim.cmd([[
-augroup netrw-auto-open-if-no-params
-  autocmd!
-  autocmd VimEnter * if argc() == 0 | Explore! | endif
-augroup END
-]])
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif]],
+    desc = "When re-open a file, place the cursor at the last position",
+})
 
--- Remove all trailing whitespace on save
-vim.api.nvim_exec(
-    [[
-  augroup trim-white-space-on-save
-    au!
-    autocmd BufWritePre * :%s/\s\+$//e
-  augroup END
-  ]],
-    false
-)
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    command = [[if argc() == 0 | Explore! | endif]],
+    desc = "If NeoVim is opened without parammeter, start NetRw on curren dir",
+})
 
--- Prevent new line to also start with a comment
-vim.api.nvim_exec(
-    [[
-  augroup disable-comments-on-new-lines
-    au!
-    au FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-  augroup END
-]],
-    false
-)
-
--- Hello
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    command = [[:%s/\s\+$//e]],
+    desc = "Remove all trailing whitespace on save",
+})
 
 -- Diagnostics info in the LSP popup. (No need to use `D`)
-vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
-
--- Material Theme
-vim.cmd([[command! MaterialToggle execute "lua require('material.functions').toggle_style()"]])
-
--- :Git opens NeoGit on the sidebar
-vim.cmd([[command! Diffsplit execute "Gitsigns diffthis"]])
+--vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
 
 -- :Tstatus command to shows Telescope's git_status()
-vim.cmd([[command! Tstatus execute "lua require(\"telescope.builtin\").git_status()"]])
-vim.cmd([[command! Tgrep execute "lua require(\"telescope.builtin\").live_grep()"]])
-vim.cmd([[command! Ttags execute "lua require(\"telescope.builtin\").help_tags()"]])
+--vim.cmd([[command! Tstatus execute "lua require(\"telescope.builtin\").git_status()"]])
+--vim.cmd([[command! Tgrep execute "lua require(\"telescope.builtin\").live_grep()"]])
+--vim.cmd([[command! Ttags execute "lua require(\"telescope.builtin\").help_tags()"]])
 
 -- :Format command to format code using LSP
-vim.cmd("command! Format execute 'lua vim.lsp.buf.formatting()'")
+--vim.cmd("command! Format execute 'lua vim.lsp.buf.format({async = true})'")
 
 -- :Actions command to execute LSP's code_action() function
-vim.cmd("command! Actions execute 'lua vim.lsp.buf.code_action()'")
+--vim.cmd("command! Actions execute 'lua vim.lsp.buf.code_action()'")
+
+vim.api.nvim_create_user_command("HolaMundo", "lua print(1234)", {})
